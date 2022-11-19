ec746a493a8f081d616bb9318e50842d9a38bcd9
Puvendhan <puvendhan@Puvendhans-MacBook-Pro.local>
2022-11-20 01:25:47 +0530

    updated the files

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_eks_cluster" "puvendhan-eks" {
 }
 
 
-resource "aws_iam_role" "workernodes" {
+resource "aws_iam_role" "eks-workernodes" {
   name = "eks-node-group-puvendhan"
  
   assume_role_policy = jsonencode({
@@ -71,12 +71,12 @@ resource "aws_iam_role" "workernodes" {
  
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role    = aws_iam_role.workernodes.name
+  role    = aws_iam_role.eks-workernodes.name
 }
  
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role    = aws_iam_role.workernodes.name
+  role    = aws_iam_role.eks-workernodes.name
 }
  
 resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
@@ -86,7 +86,7 @@ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRC
  
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role    = aws_iam_role.workernodes.name
+  role    = aws_iam_role.eks-workernodes.name
 }
 
 resource "aws_eks_node_group" "worker-node-group" {
