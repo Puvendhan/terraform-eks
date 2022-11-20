67f0387e1b487abb55a789c0cf1e637300f8d9d3
Puvendhan <puvendhan@Puvendhans-MacBook-Pro.local>
2022-11-20 15:53:42 +0530

    updated the files with backedn

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-0c97076dad30e1155"
+  default = "subnet-08b736368341d386d"
  }
  
  variable "subnet_id_2" {
   type = string
-  default = "subnet-0e1be31fb95a6db74"
+  default = "subnet-06d3f6707fdc389bd"
  }
